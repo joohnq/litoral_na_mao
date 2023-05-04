7196ac3b695d3a3ad7425b0384a1c963c44945b4
João Henrique <joaohenriquess3287@gmail.com>
2023-05-04 17:48:20 -0300

    feat: create a header prototype

---
+++ lib/components/header.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+            height: 100,
+          ),
+          const SizedBox(width: 20),
+          const Icon(Icons.menu_rounded, size: 50.0),
+        ],
+      ),
+    );
+  }
+}
